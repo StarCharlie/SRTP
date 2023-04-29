1f929e4d18bcd1c3c8be585dc54c62afbe5b3a2e
StarCharlie <101091503+StarCharlie@users.noreply.github.com>
2023-04-29 11:38:25 +0800

    Add files via upload
    添加疾病、灸法、穴位三者关联，neo4j可以查询

--- graphGenerated.py
+++ graphGenerated.py
@@ -19,6 +19,8 @@ relation_path = 'static/datas/病症穴位.xls'
 
 file_path2 = 'static/datas/灸法.xls'
 
+file_path3 = 'static/datas/外科皮肤科.xls'
+
 max_count_test = 50
 
 
@@ -26,6 +28,10 @@ max_count_test = 50
 def create_bingzheng_graph():
     bingzheng_data = pd.read_excel(file_path, sheet_name="病症", header=0)
     bingzheng_array = np.array(bingzheng_data)
+
+    bingzheng_jiufa_xuewei_data = pd.read_excel(file_path3, sheet_name="Sheet1", header=0)
+    bingzheng_jiufa_xuewei_array = np.array(bingzheng_jiufa_xuewei_data)
+    print(bingzheng_jiufa_xuewei_array)
     for i in range(0, len(bingzheng_array)):
         node = Node('disease',
                     id=bingzheng_array[i][0],
@@ -36,11 +42,46 @@ def create_bingzheng_graph():
                         name=bingzheng_array[i][1])
 
         graph.create(node)
+        print(node.values())
         graph.merge(relation, 'category', 'name')
 
         category_of_disease = Relationship(node, '属于', relation)
-        print(relation.values())
+        # print(relation.values())
         graph.create(category_of_disease)
+
+        jiufa_nodes = []
+        for j in range(0, len(bingzheng_jiufa_xuewei_array)):
+            if(bingzheng_array[i][2] == bingzheng_jiufa_xuewei_array[j][0]):
+                jiufa_name = bingzheng_jiufa_xuewei_array[j][1]
+                jiufa_node = graph.nodes.match('jiufa', name=jiufa_name).first()
+
+                if not jiufa_node:
+                    jiufa_node = Node('jiufa', name=jiufa_name)
+                    jiufa_nodes.append(jiufa_node)
+                    graph.create(jiufa_node)
+
+                graph.merge(jiufa_node, 'jiufa', 'name')
+                print(jiufa_node.values())
+
+                xuewei_nodes = []
+                xuewei_name = bingzheng_jiufa_xuewei_array[j][2]  # assume xuewei node name is in the 4th column
+                xuewei_node = graph.nodes.match('acupoint', name=xuewei_name).first()
+
+                if not xuewei_node:
+                    xuewei_node = Node('acupoint', name=xuewei_name)
+                    xuewei_nodes.append(xuewei_node)
+                    graph.create(xuewei_node)
+
+                graph.merge(xuewei_node, 'acupoint', 'name')
+                print(xuewei_node.values())
+                relation_name = '治疗'+bingzheng_array[i][2]
+                jiufa_in_xuewei = Relationship(jiufa_node, relation_name, xuewei_node)
+                graph.create(jiufa_in_xuewei)
+
+
+                jiufa_to_bingzheng = Relationship(node,'可用灸法', jiufa_node)
+                # print(jiufa_to_bingzheng.values())
+                graph.create(jiufa_to_bingzheng)
 
 
 def create_xuewei_graph():
@@ -85,6 +126,27 @@ def create_jiufa_graph():
         category_of_jiufa = Relationship(node, '包括', relation)
         graph.create(category_of_jiufa)
 
+def create_disease_jiufa_xuewei_graph():
+    disease_jiufa_xuewei_data = pd.read_excel(file_path3, sheet_name="Sheet1", header=0)
+    print(disease_jiufa_xuewei_data)
+    disease_jiufa_xuewei_data_array = np.array(disease_jiufa_xuewei_data)
+    for i in range(0, len(disease_jiufa_xuewei_data_array)):
+        node = Node('disease',
+                    name=disease_jiufa_xuewei_data_array[i][0],
+                    )
+        relation1 = Node('jiufa',
+                        name=disease_jiufa_xuewei_data_array[i][1])
+        # relation2 = Node('xuewei',
+        #                 name=disease_jiufa_xuewei_data_array[i][2])
+
+        graph.create(node)
+        graph.merge(relation1, 'jiufa', 'name')
+        # graph.merge(relation2, 'xuewei', 'name')
+
+        category_of_disease_jiufa_xuewei_data1 = Relationship(node, '治疗', relation1)
+        # category_of_disease_jiufa_xuewei_data2 = Relationship(node, '治疗', relation2)
+        graph.create(category_of_disease_jiufa_xuewei_data1)
+        # graph.create(category_of_disease_jiufa_xuewei_data2)
 
 
 def test():
@@ -101,4 +163,5 @@ if __name__ == '__main__':
     create_xuewei_graph()
     create_disease_xuewei_relation()
     create_jiufa_graph()
+    # create_disease_jiufa_xuewei_graph()
     print("create over")
