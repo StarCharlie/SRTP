db4cda3601bd5edcb08ba755a7cbef3998620e55
blt_NOP <826678738@qq.com>
2023-05-10 01:25:37 +0800

    leave dist

--- src/global/global.js
+++
@@ -1,9 +0,0 @@
-const baseURL = "http://127.0.0.1:5000";
-const neo4jUserName = 'neo4j';
-const neo4jUserPassword = 'syt20010907';
- 
-export default {
-  baseURL,
-  neo4jUserName,
-  neo4jUserPassword,
-};
